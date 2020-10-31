5a8506841d2efd104f9313fa09ad00a017cf3e25
Dongri Jin <dongrify@gmail.com>
2020-10-31 13:39:24 +0900

    Fix gke.sh

--- gke.sh
+++ gke.sh
@@ -1,51 +1,59 @@
 #!/bin/sh
 
-function create() {
+function create_cluster() {
   echo "🍺 create cluster"
   gcloud container clusters create gke \
     --machine-type=e2-micro \
     --num-nodes=1
 }
 
-function delete() {
+function delete_cluster() {
   echo "🍺 delete cluster"
   gcloud container clusters delete gke
 }
 
-function gcr() {
+function gcr_auth() {
   echo "🍺 gcr auth"
   gcloud auth configure-docker
 }
 
-function image() {
+function image_push() {
   echo "🍺 push image"
   docker build -t gke .
   docker tag gke gcr.io/dongri/gke:v1
   docker push gcr.io/dongri/gke:v1
 }
 
-function apply() {
-  echo "🍺 apply k8s"
-  kubectl apply -f k8s/
+function apply_service() {
+  echo "🍺 apply service"
+  kubectl apply -f k8s/service.yml
+}
+
+function apply_ingress() {
+  echo "🍺 apply ingress"
+  kubectl apply -f k8s/ingress.yml
 }
 
 case "$1" in
-  create)
-    create
+  create_cluster)
+    create_cluster
     ;;
-  gcr)
-    gcr
+  delete_cluster)
+    delete_cluster
     ;;
-  image)
-    image
+  gcr_auth)
+    gcr_auth
     ;;
-  apply)
-    apply
+  image_push)
+    image_push
     ;;
-  delete)
-    delete
+  apply_service)
+    apply_service
+    ;;
+  apply_ingress)
+    apply_ingress
     ;;
   *)
-    echo "$ gke.sh [ create | gcr | image | apply | delete ]"
+    echo "$ gke.sh [ create_cluster | delete_cluster | gcr_auth | image_push | apply_service | apply_ingress ]"
     ;;
 esac
